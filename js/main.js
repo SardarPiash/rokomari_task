ec46da27f3074bf69bc65a2dc2b8e5e03437acac
Sardar Piash <piashm66@gmail.com>
2024-11-03 23:46:03 +0600

    s

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// global variable 
+let totalParchaseAmount = 700
+let trackOfferDiv
+let selectAllDiv
+
+
 // discount on offer track
 // data
 const discountData = [
@@ -9,23 +15,27 @@ const discountData = [
 
 // function to print discount data
 function DiscountAmount(){
-    const trackOfferDiv = document.getElementById("track-offer");
-    trackOfferDiv.innerHTML=''
+    trackOfferDiv = document.getElementById("track-offer");
+    trackOfferDiv.innerHTML=`<div class="discount-container">
+    <span class="discount-text">Your total</span>
+    <span class="discount-on-order">ট${totalParchaseAmount}</span>
+    </div>`;
 
     if(discountData){
+        
         discountData.forEach((data)=>{
             let value 
             if(data.discount === "free"){
               value = "FREE SHIPPING";
             }else{
-              value = `t${data.discount} OFF`;
+              value = `ট${data.discount} OFF`;
             }
             const childDiv = document.createElement('div');
             childDiv.classList.add('discount-container')
             
             childDiv.innerHTML=`
               <span class="discount-text">${value}</span>
-              <span class="discount-on-order">Order t${data.order} OFF</span>
+              <span class="discount-on-order">Order ট${data.order}</span>
             `;
 
             trackOfferDiv.appendChild(childDiv);
@@ -35,6 +45,40 @@ function DiscountAmount(){
 
 }
 
+
+// Select all container
+// function to handle select all container
+function SelectAll(){
+    selectAllDiv = document.getElementById("select-all-container");
+    selectAllDiv.innerHTML='';
+    const selectDivElements = document.createElement('div');
+    selectDivElements.classList.add('select-elements');
+    
+    selectDivElements.innerHTML=`
+    <div class="select-checkbox">
+    <span><input type="checkbox" name="checkbox" value="" /></span>
+    <span>SelectAll(2 items)</span>
+    </div>
+    <div class="select-total-amount"> 
+    <span>Your Total:<span class="select-discount-text">tk600</span>tk700</span>
+    </div>
+    `;
+    selectAllDiv.appendChild(selectDivElements);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     DiscountAmount();
+    SelectAll();
 })
