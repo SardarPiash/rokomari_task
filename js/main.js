3b13ceb030beddd99f5e77282ffcb84a7751550c
Sardar Piash <piashm66@gmail.com>
2024-11-04 22:56:47 +0600

    total fix

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ let totalParchaseAmount = 700;
 let trackOfferDiv;
 let selectAllDiv;
 let orderListDiv;
-let productQuantity
-let tempDisPrice = 10
-let tempOriPrice = 10
+let productQuantity;
+let tempDisPrice = 10;
+let tempOriPrice = 10;
 let selectAllState = true;
-
 
 // discount on offer track
 // data
@@ -81,13 +80,18 @@ function SelectAll() {
     <span>SelectAll(2 items)</span>
     </div>
     <div class="select-total-amount"> 
-    <span>Your Total:<span class="select-discount-text">ট 0 ট 0 </span></span>
-    </div>
+    <span>Your Total: 
+        <span class="select-discount-text">
+            <span class="original-price">ট 0</span>
+            <span class="discounted-price"> ট 0</span>
+        </span>
+    </span>
+</div>
+
     `;
 
-   
   selectAllDiv.appendChild(selectDivElements);
-  let checkedb = document.getElementById('select-all-input');
+  let checkedb = document.getElementById("select-all-input");
   checkedb.checked = selectAllState;
 }
 
@@ -97,7 +101,7 @@ function OrderList() {
   orderListDiv = document.getElementById("order-list-container");
   orderListDiv.innerHTML = "";
   const orderConfimDiv = document.createElement("div");
-  orderConfimDiv.classList.add('order-confirm-div');
+  orderConfimDiv.classList.add("order-confirm-div");
   orderConfimDiv.innerHTML = `<span><button class="order-button-gift">Order as a Gift</button></span>
   <span><button class="order-button-shipping">Go to Shipping Page</button></span>
   `;
@@ -157,15 +161,16 @@ function OrderList() {
 const addressData = {
   area: "Mirpur-1",
   name: "Abu Shaleh Md. Kaium",
-  place:"Home",
+  place: "Home",
   phone: "01627754406",
-  fullAddress: "Flat#157/2/A, Ahamed Nagar, Paikepara, Mirpur-1, Dhaka, Bangladesh ",
+  fullAddress:
+    "Flat#157/2/A, Ahamed Nagar, Paikepara, Mirpur-1, Dhaka, Bangladesh ",
 };
-function CustomerAddress(addressData){
-        const customerAddressDiv = document.getElementById('customer-address');
-       // customerAddressDiv.classList.add('customer-address-container')
-        const address = document.createElement('div');
-        address.innerHTML = `
+function CustomerAddress(addressData) {
+  const customerAddressDiv = document.getElementById("customer-address");
+  // customerAddressDiv.classList.add('customer-address-container')
+  const address = document.createElement("div");
+  address.innerHTML = `
         <div class="customer-address-container">
         <div class="customer-address">
             <div class="address-location">
@@ -189,66 +194,58 @@ function CustomerAddress(addressData){
         </div>
         `;
 
-        customerAddressDiv.appendChild(address)
+  customerAddressDiv.appendChild(address);
 }
 
-
-
-
-
-
-
-
 // handle select all function
-function handleSelectAll(){
-  selectAllState = !selectAllState
-  console.log(selectAllState)
-  ProductInputCheckbox()
+function handleSelectAll() {
+  selectAllState = !selectAllState;
+  console.log(selectAllState);
+  ProductInputCheckbox();
   handleYourTotal();
 }
 
 // checked uncheck checking
-function ProductInputCheckbox(){
-  const checkbox = document.querySelectorAll('.product-checkbox');
-  checkbox.forEach((check)=>{
-    if(selectAllState){
+function ProductInputCheckbox() {
+  const checkbox = document.querySelectorAll(".product-checkbox");
+  checkbox.forEach((check) => {
+    if (selectAllState) {
       check.checked = true;
-    }else{
+    } else {
       check.checked = false;
     }
-  })
-
+  });
 }
 
 // handleProductQuantity
-function handleProductQuantity(index,value){
-  const products = document.querySelectorAll('.order-quantity-number');
+function handleProductQuantity(index, value) {
+  const products = document.querySelectorAll(".order-quantity-number");
   const product = products[index];
-  productQuantity = parseInt(product.innerHTML,10);
+  productQuantity = parseInt(product.innerHTML, 10);
 
-  const productsPrice = document.querySelectorAll('.price-container');
-  const availability = document.querySelectorAll('.availability');
-  const available  = availability[index];
+  const productsPrice = document.querySelectorAll(".price-container");
+  const availability = document.querySelectorAll(".availability");
+  const available = availability[index];
   const productPrice = productsPrice[index];
   tempDisPrice = orderListData[index].discounted_price;
   tempOriPrice = orderListData[index].original_price;
   let bookAvailabe = orderListData[index].availability;
-// book quantity control
-  if(value){
-    if(bookAvailabe > 0){
+  // book quantity control
+  if (value) {
+    if (bookAvailabe > 0) {
       productQuantity = productQuantity + 1;
       bookAvailabe = bookAvailabe - 1;
     }
-  }else{
-    if(productQuantity > 1){
+  } else {
+    if (productQuantity > 1) {
       productQuantity = productQuantity - 1;
       bookAvailabe = bookAvailabe + 1;
     }
   }
   orderListData[index].availability = bookAvailabe;
-// price calculation
-  tempDisPrice = tempDisPrice *  productQuantity;
-  tempOriPrice = tempOriPrice *  productQuantity;
+  // price calculation
+  tempDisPrice = tempDisPrice * productQuantity;
+  tempOriPrice = tempOriPrice * productQuantity;
   const newPrice = `<span class="price-discount">tk${tempDisPrice}</span>
                     <span class="price-original">tk${tempOriPrice}</span>`;
 
@@ -262,45 +259,52 @@ function handleProductQuantity(index,value){
 function handleYourTotal() {
   let totalOriginalPrice = 0;
   let totalDiscountPrice = 0;
-  let flag = false;
-  
-  const productCheckboxDiv = document.querySelectorAll('.product-checkbox');
-  const productQuantities = document.querySelectorAll('.order-quantity-number');
+  let flag = [];
+
+  const productCheckboxDiv = document.querySelectorAll(".product-checkbox");
+  const productQuantities = document.querySelectorAll(".order-quantity-number");
 
   productCheckboxDiv.forEach((checkbox, index) => {
-    flag = checkbox.checked
+    flag.push(checkbox.checked);
     if (checkbox.checked) {
-      
-
       let productQuantity = parseInt(productQuantities[index].innerHTML, 10);
-      
-      const originalPrice = orderListData[index].original_price * productQuantity;
-      const discountedPrice = orderListData[index].discounted_price * productQuantity;
-      
+
+      const originalPrice =
+        orderListData[index].original_price * productQuantity;
+      const discountedPrice =
+        orderListData[index].discounted_price * productQuantity;
+
       // Add to total
       totalOriginalPrice = totalOriginalPrice + originalPrice;
       totalDiscountPrice = totalDiscountPrice + discountedPrice;
     }
   });
-  
-  // select all input become true or false
-  if(flag){
-    selectAllState = true
-    let checkedb = document.getElementById('select-all-input');
-    checkedb.checked = selectAllState;
-  }else{
-    selectAllState = false
-    let checkedb = document.getElementById('select-all-input');
-    checkedb.checked = selectAllState;
+
+  //handle select checked
+  for (let i = 0; i < flag.length; i++) {
+    console.log(flag[i])
+    if (flag[i]) {
+      selectAllState = true;
+      let checkedb = document.getElementById("select-all-input");
+      checkedb.checked = selectAllState;
+    } else {
+      selectAllState = false;
+      let checkedb = document.getElementById("select-all-input");
+      checkedb.checked = selectAllState;
+      break;
+    }
   }
-  document.querySelector('.select-total-amount .select-discount-text').innerHTML = `ট${totalDiscountPrice}`;
-  document.querySelector('.select-total-amount').lastChild.textContent = ` ট${totalOriginalPrice}`;
+  
+
+  document.querySelector(
+    ".select-total-amount .select-discount-text"
+  ).innerHTML = `
+  <span class="original-price">ট${totalDiscountPrice}</span>
+  <span class="discounted-price"> ট${totalOriginalPrice}</span>`;
+  //document.querySelector('.select-total-amount').lastChild.textContent = ` ট${totalOriginalPrice}`;
 }
 
 //
-
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
   DiscountAmount();
